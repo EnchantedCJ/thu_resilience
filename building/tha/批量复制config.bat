af19723f2219c628e41443074f95cbd733c244cb
EnchantedCJ <sunchujin@qq.com>
2019-09-27 12:03:30 +0800

    Delete some files, modify gitignore

--- building/tha/批量复制config.bat
+++
@@ -1,8 +0,0 @@
-xcopy /y "%~dp0config.txt" .\0.2\x\inputs
-xcopy /y "%~dp0config.txt" .\0.2\y\inputs
-xcopy /y "%~dp0config.txt" .\0.3\x\inputs
-xcopy /y "%~dp0config.txt" .\0.3\y\inputs
-xcopy /y "%~dp0config.txt" .\0.4\x\inputs
-xcopy /y "%~dp0config.txt" .\0.4\y\inputs
-
-pause
